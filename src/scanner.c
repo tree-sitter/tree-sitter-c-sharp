058edeb3785255e245ca0dcbeff3c5303ebba911
Amaan Qureshi <amaanq12@gmail.com>
2024-04-30 14:58:28 -0400

    feat!: rewrite the grammar to support missing features & reduce state count

--- src/scanner.c
+++ src/scanner.c
@@ -1,37 +1,357 @@
-#include <tree_sitter/parser.h>
+#include "tree_sitter/alloc.h"
+#include "tree_sitter/array.h"
+#include "tree_sitter/parser.h"
+
 #include <wctype.h>
 
 enum TokenType {
-  PREPROC_DIRECTIVE_END,
-  OPT_SEMI,
+    OPT_SEMI,
+    INTERPOLATION_REGULAR_START,
+    INTERPOLATION_VERBATIM_START,
+    INTERPOLATION_RAW_START,
+    INTERPOLATION_START_QUOTE,
+    INTERPOLATION_END_QUOTE,
+    INTERPOLATION_OPEN_BRACE,
+    INTERPOLATION_CLOSE_BRACE,
+    INTERPOLATION_STRING_CONTENT,
 };
 
-void *tree_sitter_c_sharp_external_scanner_create() { return NULL; }
-void tree_sitter_c_sharp_external_scanner_destroy(void *p) {}
-void tree_sitter_c_sharp_external_scanner_reset(void *p) {}
-unsigned tree_sitter_c_sharp_external_scanner_serialize(void *p, char *buffer) { return 0; }
-void tree_sitter_c_sharp_external_scanner_deserialize(void *p, const char *b, unsigned n) {}
+typedef enum {
+    REGULAR,
+    VERBATIM,
+    RAW,
+} StringType;
 
-bool tree_sitter_c_sharp_external_scanner_scan(
-  void *payload,
-  TSLexer *lexer,
-  const bool *valid_symbols
-) {
-  if (valid_symbols[OPT_SEMI]) {
-    lexer->result_symbol = OPT_SEMI;
-    if (lexer->lookahead == ';') {
-      lexer->advance(lexer, true);
+typedef struct {
+    uint8_t dollar_count;
+    uint8_t open_brace_count;
+    uint8_t quote_count;
+    StringType string_type;
+} Interpolation;
+
+typedef struct {
+    Array(Interpolation) interpolation_stack;
+} Scanner;
+
+static inline void advance(TSLexer *lexer) { lexer->advance(lexer, false); }
+
+static inline void skip(TSLexer *lexer) { lexer->advance(lexer, true); }
+
+void *tree_sitter_c_sharp_external_scanner_create() {
+    Scanner *scanner = ts_calloc(sizeof(Scanner), 1);
+    array_init(&scanner->interpolation_stack);
+    return scanner;
+}
+
+void tree_sitter_c_sharp_external_scanner_destroy(void *payload) {
+    Scanner *scanner = (Scanner *)payload;
+    array_delete(&scanner->interpolation_stack);
+    ts_free(scanner);
+}
+
+unsigned tree_sitter_c_sharp_external_scanner_serialize(void *payload, char *buffer) {
+    Scanner *scanner = (Scanner *)payload;
+
+    if (scanner->interpolation_stack.size * 4 > TREE_SITTER_SERIALIZATION_BUFFER_SIZE) {
+        return 0;
     }
-    return true;
-  }   
-  // Detect either a newline or EOF. Currently, external scanners
-  // are the only way to match against EOF.
-  if (!valid_symbols[PREPROC_DIRECTIVE_END]) return false;
-  lexer->result_symbol = PREPROC_DIRECTIVE_END;
-  for (;;) {
-    if (lexer->lookahead == 0) return true;
-    if (lexer->lookahead == '\n') return true;
-    if (!iswspace(lexer->lookahead)) return false;
-    lexer->advance(lexer, true);
-  }
+
+    unsigned size = 0;
+
+    buffer[size++] = (char)scanner->interpolation_stack.size;
+
+    for (unsigned i = 0; i < scanner->interpolation_stack.size; i++) {
+        Interpolation interpolation = scanner->interpolation_stack.contents[i];
+        buffer[size++] = (char)interpolation.dollar_count;
+        buffer[size++] = (char)interpolation.open_brace_count;
+        buffer[size++] = (char)interpolation.quote_count;
+        buffer[size++] = (char)interpolation.string_type;
+    }
+
+    return size;
+}
+
+void tree_sitter_c_sharp_external_scanner_deserialize(void *payload, const char *buffer, unsigned length) {
+    Scanner *scanner = (Scanner *)payload;
+
+    array_clear(&scanner->interpolation_stack);
+    unsigned size = 0;
+
+    if (length > 0) {
+        scanner->interpolation_stack.size = (unsigned char)buffer[size++];
+        array_reserve(&scanner->interpolation_stack, scanner->interpolation_stack.size);
+
+        for (unsigned i = 0; i < scanner->interpolation_stack.size; i++) {
+            Interpolation interpolation = {0};
+            interpolation.dollar_count = buffer[size++];
+            interpolation.open_brace_count = buffer[size++];
+            interpolation.quote_count = buffer[size++];
+            interpolation.string_type = (unsigned char)buffer[size++];
+            scanner->interpolation_stack.contents[i] = interpolation;
+        }
+    }
+
+    assert(size == length);
+}
+
+bool tree_sitter_c_sharp_external_scanner_scan(void *payload, TSLexer *lexer, const bool *valid_symbols) {
+    Scanner *scanner = (Scanner *)payload;
+
+    uint8_t brace_advanced = 0;
+    bool did_advance = false;
+
+    // error recovery, gives better trees this way
+    if (valid_symbols[OPT_SEMI] && valid_symbols[INTERPOLATION_REGULAR_START]) {
+        return false;
+    }
+
+    if (valid_symbols[OPT_SEMI]) {
+        lexer->result_symbol = OPT_SEMI;
+        if (lexer->lookahead == ';') {
+            advance(lexer);
+        }
+        return true;
+    }
+
+    if (valid_symbols[INTERPOLATION_REGULAR_START] || valid_symbols[INTERPOLATION_VERBATIM_START] ||
+        valid_symbols[INTERPOLATION_RAW_START]) {
+        while (iswspace(lexer->lookahead)) {
+            skip(lexer);
+        }
+
+        uint8_t dollar_advanced = 0;
+
+        bool is_verbatim = false;
+
+        if (lexer->lookahead == '@') {
+            is_verbatim = true;
+            advance(lexer);
+        }
+
+        while (lexer->lookahead == '$') {
+            advance(lexer);
+            dollar_advanced++;
+        }
+
+        if (dollar_advanced > 0 && (lexer->lookahead == '"' || lexer->lookahead == '@')) {
+            lexer->result_symbol = INTERPOLATION_REGULAR_START;
+            Interpolation interpolation = {
+                .dollar_count = dollar_advanced,
+                .open_brace_count = 0,
+                .quote_count = 0,
+                .string_type = REGULAR,
+            };
+
+            if (is_verbatim || lexer->lookahead == '@') {
+                if (lexer->lookahead == '@') {
+                    advance(lexer);
+                }
+                lexer->result_symbol = INTERPOLATION_VERBATIM_START;
+                interpolation.string_type = VERBATIM;
+                array_push(&scanner->interpolation_stack, interpolation);
+            } else {
+                lexer->mark_end(lexer);
+                advance(lexer);
+                if (lexer->lookahead == '"') {
+                    advance(lexer);
+                    if (lexer->lookahead == '"') {
+                        lexer->result_symbol = INTERPOLATION_RAW_START;
+                        interpolation.string_type = RAW;
+                        array_push(&scanner->interpolation_stack, interpolation);
+                    }
+                    // If we find 1 or 3 quotes, we push an interpolation.
+                    // If there's only two quotes, that's just an empty string
+                } else {
+                    array_push(&scanner->interpolation_stack, interpolation);
+                }
+            }
+            return true;
+        }
+    }
+
+    if (valid_symbols[INTERPOLATION_START_QUOTE] && scanner->interpolation_stack.size > 0) {
+        Interpolation *current_interpolation = array_back(&scanner->interpolation_stack);
+
+        while (lexer->lookahead == '"') {
+            advance(lexer);
+            current_interpolation->quote_count++;
+        }
+
+        lexer->result_symbol = INTERPOLATION_START_QUOTE;
+        return current_interpolation->quote_count >= 3;
+    }
+
+    if (valid_symbols[INTERPOLATION_END_QUOTE] && scanner->interpolation_stack.size > 0) {
+        Interpolation *current_interpolation = array_back(&scanner->interpolation_stack);
+        uint8_t quote_advanced = 0;
+
+        while (lexer->lookahead == '"') {
+            advance(lexer);
+            quote_advanced++;
+        }
+
+        if (quote_advanced == current_interpolation->quote_count) {
+            lexer->result_symbol = INTERPOLATION_END_QUOTE;
+            array_pop(&scanner->interpolation_stack);
+            return true;
+        }
+
+        did_advance = quote_advanced > 0;
+    }
+
+    if (valid_symbols[INTERPOLATION_OPEN_BRACE] && scanner->interpolation_stack.size > 0) {
+        Interpolation *current_interpolation = array_back(&scanner->interpolation_stack);
+
+        while (lexer->lookahead == '{' && brace_advanced < current_interpolation->dollar_count) {
+            advance(lexer);
+            brace_advanced++;
+        }
+
+        if (brace_advanced > 0 && brace_advanced == current_interpolation->dollar_count &&
+            (brace_advanced == 0 || lexer->lookahead != '{')) {
+            current_interpolation->open_brace_count = brace_advanced;
+            lexer->result_symbol = INTERPOLATION_OPEN_BRACE;
+            return true;
+        }
+    }
+
+    if (valid_symbols[INTERPOLATION_CLOSE_BRACE] && scanner->interpolation_stack.size > 0) {
+        uint8_t brace_advanced = 0;
+        Interpolation *current_interpolation = array_back(&scanner->interpolation_stack);
+
+        while (iswspace(lexer->lookahead)) {
+            advance(lexer);
+        }
+
+        while (lexer->lookahead == '}') {
+            advance(lexer);
+            brace_advanced++;
+
+            if (brace_advanced == current_interpolation->open_brace_count) {
+                current_interpolation->open_brace_count = 0;
+                if (lexer->lookahead == '"') {
+                    if (current_interpolation->string_type == REGULAR ||
+                        current_interpolation->string_type == VERBATIM) {
+                        array_pop(&scanner->interpolation_stack);
+                    } else {
+                        lexer->mark_end(lexer);
+                        advance(lexer);
+                        if (lexer->lookahead == '"') {
+                            advance(lexer);
+                            uint8_t quote_advanced = 2;
+                            while (lexer->lookahead == '"') {
+                                quote_advanced++;
+                            }
+                            if (quote_advanced == current_interpolation->quote_count) {
+                                array_pop(&scanner->interpolation_stack);
+                            }
+                        }
+                    }
+                }
+                lexer->result_symbol = INTERPOLATION_CLOSE_BRACE;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    if (valid_symbols[INTERPOLATION_STRING_CONTENT] && scanner->interpolation_stack.size > 0) {
+        lexer->result_symbol = INTERPOLATION_STRING_CONTENT;
+        Interpolation *current_interpolation = array_back(&scanner->interpolation_stack);
+
+        while (lexer->lookahead) {
+            if (current_interpolation->string_type == REGULAR) {
+                if (lexer->lookahead == '\\' || lexer->lookahead == '\n' || lexer->lookahead == '"') {
+                    if (did_advance && lexer->lookahead == '"') {
+                        array_pop(&scanner->interpolation_stack);
+                    }
+                    lexer->mark_end(lexer);
+                    return did_advance;
+                }
+
+                if (lexer->lookahead == '{') {
+                    lexer->mark_end(lexer);
+
+                    while (lexer->lookahead == '{' && brace_advanced < current_interpolation->open_brace_count) {
+                        advance(lexer);
+                        brace_advanced++;
+                    }
+
+                    if (brace_advanced == current_interpolation->open_brace_count &&
+                        (brace_advanced == 0 || lexer->lookahead != '{')) { // if we're in a brace we're not allowed to
+                                                                            // collect more than the open_brace_count
+                        return did_advance;
+                    }
+                }
+            }
+
+            if (current_interpolation->string_type == VERBATIM) {
+                if (lexer->lookahead == '"') {
+                    if (did_advance) {
+                        array_pop(&scanner->interpolation_stack);
+                    }
+                    lexer->mark_end(lexer);
+                    return did_advance;
+                }
+
+                if (lexer->lookahead == '{') {
+                    lexer->mark_end(lexer);
+
+                    while (lexer->lookahead == '{' && brace_advanced < current_interpolation->open_brace_count) {
+                        advance(lexer);
+                        brace_advanced++;
+                    }
+
+                    if (brace_advanced == current_interpolation->open_brace_count &&
+                        (brace_advanced == 0 || lexer->lookahead != '{')) {
+                        return did_advance;
+                    }
+                }
+            }
+
+            if (current_interpolation->string_type == RAW) {
+                if (lexer->lookahead == '"') {
+                    lexer->mark_end(lexer);
+                    advance(lexer);
+                    if (lexer->lookahead == '"') {
+                        advance(lexer);
+                        uint8_t quote_advanced = 2;
+                        while (lexer->lookahead == '"') {
+                            quote_advanced++;
+                            advance(lexer);
+                        }
+                        if (quote_advanced == current_interpolation->quote_count) {
+                            return did_advance;
+                        }
+                    }
+                }
+
+                if (lexer->lookahead == '{') {
+                    lexer->mark_end(lexer);
+
+                    while (lexer->lookahead == '{' && brace_advanced < current_interpolation->open_brace_count) {
+                        advance(lexer);
+                        brace_advanced++;
+                    }
+
+                    if (brace_advanced == current_interpolation->open_brace_count &&
+                        (brace_advanced == 0 || lexer->lookahead != '{')) {
+                        return did_advance;
+                    }
+                }
+            }
+
+            if (lexer->lookahead != '{') {
+                brace_advanced = 0;
+            }
+            advance(lexer);
+            did_advance = true;
+        }
+
+        lexer->mark_end(lexer);
+        return did_advance;
+    }
+
+    return false;
 }

6e9cc7cdf4d9202bfef17852a7ec1d0ea70ac404
Amaan Qureshi <amaanq12@gmail.com>
2024-09-01 05:48:46 -0400

    build: update bindings

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,8 +3,8 @@ package tree_sitter_c_sharp_test
 import (
 	"testing"
 
-	tree_sitter "github.com/smacker/go-tree-sitter"
-	"github.com/tree-sitter/tree-sitter-c_sharp"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+	tree_sitter_c_sharp "github.com/tree-sitter/tree-sitter-c_sharp/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
